2f1ea49d0dd329f7d2739155a6932aa25e5e61e3
CarmoAgilar <dcarmonaa1900@alumno.ipn.mx>
2023-03-31 12:30:09 -0600

    lab2

---
+++ pythonpt2.py
@@ -0,0 +1,12 @@
+""" ESTE ES JUN SUPERCOMENTARIO
+    DE INICIO A NUESTRO RESUMEN
+"""
+#============================
+# Carmona Aguilar Diego
+#     Curso python 2
+#============================
+
+
+
+
+
