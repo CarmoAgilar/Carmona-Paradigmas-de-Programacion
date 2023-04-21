27ce2a08abb096b588d7ea051fb066e614776e05
CarmoAgilar <dcarmonaa1900@alumno.ipn.mx>
2023-04-21 12:50:40 -0600

    lab3

--- pythonpt3.py
+++ pythonpt3.py
@@ -195,3 +195,27 @@ def greet():
     print("Hello", name)
 
 greet()
+
+#_______________________________________________________
+#|                                                      |
+#|                  Inicio de Algoritmo                 |
+#|______________________________________________________|
+# Serie exponencial
+# Factorizacion de x
+# Negativos con funcion inversa 
+
+n = 200
+x = -100.0
+flag = False
+if x<0:
+    flag = True
+    x = -x
+s = 1.0
+for i in range(n,0,-1):
+    s *= x/float(i)
+    s+= 1.0
+if flag == True:
+    s = 1/s
+print(s)
+
+
