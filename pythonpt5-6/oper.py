e781e7d24bf865ac0d6eec789c49d8553040e59a
CarmoAgilar <dcarmonaa1900@alumno.ipn.mx>
2023-06-09 12:16:02 -0600

    Carpeta renombrada

---
+++ pythonpt5-6/oper.py
@@ -0,0 +1,6 @@
+#===================================
+# de functions.py trae las funciones
+#===================================
+from functions import suma, promedio
+print(suma(100,200))
+print(promedio(100,200))
