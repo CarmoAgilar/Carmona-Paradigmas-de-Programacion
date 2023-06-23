9dd7cf3f83ea79a27953055081a8555ef5a9a4ce
CarmoAgilar <dcarmonaa1900@alumno.ipn.mx>
2023-06-23 13:28:16 -0600

    lab 11

---
+++ PARALELO/NUMBA/ejemplo1.py
@@ -0,0 +1,37 @@
+from numba import jit
+import numpy
+import time
+
+@jit(nonpython=True)
+def rapido(a):
+    t = 0.0
+    for i in range(a.shape[0]):
+        t += numpy.tanh(a[i,i])
+    return a+t
+
+# con el interprete
+def lento(a):
+    t = 0.0
+    for i in range(a.shape[0]):
+        t += numpy.tanh(a[i,i])
+    return a+t
+
+x = numpy.arange(10000).reshape(100,100)
+
+# la primera llamada incluye el tiempo de compilacion
+start = time.time()
+rapido(x)
+end = time.time()
+print("Tiempo incluyendo compilacion = %s" % (end-start))
+
+# y sin tiempo de compilacion
+start = time.time()
+rapido(x)
+end = time.time()
+print("Tiempo usando numba = %s" % (end-start))
+
+# funcion lenta
+start = time.time()
+lento(x)
+end = time.time()
+print("Tiempo en python puro = %s" % (end-start))
